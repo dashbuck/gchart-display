fd8fbe0b659c7a4e5ad01e81c0656ba8c3c955d7
Dash Buck <em.dash.buck@gmail.com>
2017-06-07 15:52:24 -0700

    Okay, I think I understand how the charts work. Next step: dashboards.

--- inc/js/gchart.php
+++ inc/js/gchart.php
@@ -2,43 +2,62 @@
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
   // Load the Visualization API and the corechart package; set a callback to run when the API is loaded.
-  google.charts.load('current', { packages: [ 'corechart'], callback: drawVisualization });
+  google.charts.load('current', { packages: [ 'corechart', 'table'] });
 
-  /*Might need a more complicated callback in the future, like this:
-  google.charts.setOnLoadCallback(drawChart1);
-  google.charts.setOnLoadCallback(drawChart2);
-
- //OR
+//Set callbacks to run when the API is loaded
 
   google.charts.setOnLoadCallback(
-    function() { // Anonymous function that calls drawChart1 and drawChart2
-      drawChart1();
-      drawChart2();
+    function() { // Anonymous function that calls our callbacks
+      drawTable();
+      drawPie();
     });
-  */
 
-  // Callback that creates and populates a data table,
-  // instantiates the chart, passes in the data and
-  // draws it.
-  function drawVisualization() {
+  // Get the titles of each poem
+  function drawTable() {
     var query = new google.visualization.Query(
         'https://docs.google.com/spreadsheets/d/1TWQ11MrXIGnVjoc76iV8uI9GEUZCR9a-R4nMQWil52k/gviz/tq?headers=1');
 
     // Apply query language statement.
-    query.setQuery('SELECT A,C ORDER BY A');
+   query.setQuery('SELECT A ORDER BY A');
 
     // Send the query with a callback function.
     query.send(handleQueryResponse);
-  }
 
-  function handleQueryResponse(response) {
-    if (response.isError()) {
-      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-      return;
+    //If something goes wrong with the query, we want to know about it
+    //Monkey'd from API docs
+    function handleQueryResponse(response) {
+      if (response.isError()) {
+        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+      }
+      var data = response.getDataTable();
+      table = new google.visualization.Table(document.getElementById('poetry-table'));
+      table.draw(data, {width: '100%', height: '100%'});
     }
-
-    var data = response.getDataTable();
-    visualization = new google.visualization.LineChart(document.getElementById('visualization'));
-    visualization.draw(data, {legend: 'bottom'});
   }
+
+  // Get the distribution of line amounts
+  function drawPie() {
+    var query = new google.visualization.Query(
+        'https://docs.google.com/spreadsheets/d/1TWQ11MrXIGnVjoc76iV8uI9GEUZCR9a-R4nMQWil52k/gviz/tq?headers=1');
+
+    // Apply query language statement.
+   query.setQuery('SELECT count(D) group by C pivot E');
+
+    // Send the query with a callback function.
+    query.send(handleQueryResponse);
+
+    //If something goes wrong with the query, we want to know about it
+    //Monkey'd from API docs
+    function handleQueryResponse(response) {
+      if (response.isError()) {
+        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+      }
+      var data = response.getDataTable();
+      pie = new google.visualization.PieChart(document.getElementById('poetry-pie'));
+      pie.draw(data, {width: '100%', height: '100%'});
+    }
+  }
+
 </script>
--- index.php
+++ index.php
@@ -4,9 +4,12 @@
 ?>
 <article class="mw9 center ph3 ph4-ns dt-l w-100 mv4">
   <!--We have a chart on this page-->
-  <? include_once($path."/gchart-display/inc/js/gchart.php"); ?>
-  <!--Div that will hold the chart-->
-    <div id="visualization" class="ba b--near-black bg-white"></div>
+  <? include_once($path."/gchart-display/inc/js/gdashboard.php"); ?>
+  <!--Div that will hold the charts-->
+    <div id="dashboard_div" class="ba b--near-black bg-white">
+      <div id="filter_div"></div>
+      <div id="chart_div"></div>
+    </div>
 </article>
 <?php
   include_once($path."/gchart-display/template/footer.php");
