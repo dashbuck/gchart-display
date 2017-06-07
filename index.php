6ab328ba981e2f860e0024738e0581a00f2e8a92
Dash Buck <em.dash.buck@gmail.com>
2017-06-07 11:43:29 -0700

    Set up basic template so I don't have to stare at white space while I'm working

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  $path = $_SERVER['DOCUMENT_ROOT'];
+  include_once($path."/gchart-display/template/header.php");
+?>
+<article class="mw9 center ph3 ph4-ns dt-l w-100 mv3">
+  Hey, I'm gchart.
+</article>
+<?php
+  include_once($path."/gchart-display/template/footer.php");
+?>
---
+++ template/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Google Charts</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="https://unpkg.com/tachyons@4.7.0/css/tachyons.min.css">
+        <script src="http://localhost:35729/livereload.js"></script>
+    </head>
+    <body class="near-black bg-near-white f3-l">
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <header class="tc pt2 cover bg-center w-100 bg-light-blue">
+        <a href="/" class="link black bg-animate hover-bg-white mt2 mb0 fw1 f1" property="title">Google Charts</a>
+        <nav class="bb b--near-black bw2 tc center mt2">
+          <a class="link black bg-animate hover-bg-white dib pa2 ph4-l" href="#">Home</a>
+        </nav>
+        </header>
