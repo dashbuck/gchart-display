ae216d8607e38cffba60e257170141bcb1f06236
Dash Buck <em.dash.buck@gmail.com>
2017-06-07 13:05:43 -0700

    Proof of concept with example data, template cleanup

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
   $path = $_SERVER['DOCUMENT_ROOT'];
   include_once($path."/gchart-display/template/header.php");
 ?>
-<article class="mw9 center ph3 ph4-ns dt-l w-100 mv3">
-  Hey, I'm gchart.
+<article class="mw9 center ph3 ph4-ns dt-l w-100 mv4">
+  <!--We have a chart on this page-->
+  <? include_once($path."/gchart-display/inc/js/gchart.php"); ?>
+  <!--Div that will hold the chart-->
+    <div id="chart_div" class="ba b--near-black bg-white"></div>
 </article>
 <?php
   include_once($path."/gchart-display/template/footer.php");
--- template/header.php
+++ template/header.php
@@ -16,8 +16,5 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <header class="tc pt2 cover bg-center w-100 bg-light-blue">
-        <a href="/" class="link black bg-animate hover-bg-white mt2 mb0 fw1 f1" property="title">Google Charts</a>
-        <nav class="bb b--near-black bw2 tc center mt2">
-          <a class="link black bg-animate hover-bg-white dib pa2 ph4-l" href="#">Home</a>
-        </nav>
+          <!--nav goes here if I need one later-->
         </header>
